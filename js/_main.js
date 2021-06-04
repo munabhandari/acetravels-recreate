bfbaf384876de9b30fcdebade74fc085e29645fd
muna bhandari <hotline.muna@gmail.com>
2021-06-04 19:46:28 +0545

    sliders added

---
+++ js/_main.js
@@ -0,0 +1,49 @@
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+var navbar = document.getElementById("top-header");
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= 100) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+// Scroll top function with smooth transition
+var gotoTopIcon = document.querySelector('#goto-top-icon');
+gotoTopIcon.addEventListener('click', function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0;
+});
+
+
+// review-slider done using owlCarousel 
+$(document).ready(function () {
+    $(".review-slider").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplaySpeed: 500,
+        autoplayTimeout: 3000,
+        nav: true,
+        autoplay: true
+    });
+});
+
+
+// image-slider done using owlCarousel 
+$(document).ready(function () {
+    $(".image-slider").owlCarousel({
+        items: 3,
+        loop: true,
+        autoplaySpeed: 500,
+        autoplayTimeout: 3000,
+        nav: true,
+        autoplay: true
+    });
+});
